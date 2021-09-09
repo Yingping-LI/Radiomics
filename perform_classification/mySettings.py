bd74d11e379e81e57d248257d52605a2aa8c127c
liyingping <yingpingleee@126.com>
2021-09-09 18:41:57 +0200

    save classification codes (not arranged well)

--- perform_classification/mySettings.py
+++ perform_classification/mySettings.py
@@ -1,16 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env pythonhttp://localhost:8888/edit/2020_MRI_Work/Radiogenomics/perform_classification/mySettings.py#
 # coding: utf-8
 import pandas as pd
 import os
+
+#========================================
+# - "experiment_class": {"BraTS2021",  "TCGA_IDH",  "TCGA_MGMT"}, used to control different datasets for experiments.
+# - "task_name_list": list some tasks in the specified "experiment_class", if it is a empty list, 
+#                     then all the tasks in this experiment_class will be done!
 
 '''
 Basic Settings for the code
 '''
 global_basic_settings={
-    "experiment_class": "TCGA_MGMT", #"BraTS2021",  "TCGA_IDH",  "TCGA_MGMT"
-    ##"501.01_BraTS2021-segNiiData_base", "601.01_BraTS2021-dcmToNiiData_base",  "701.01_BraTS2021-segNiiData-zscore_base"
-    "task_name":"TCGA_4.01_isMGMTMethylated_base", 
+    "experiment_class": "BraTS2021", #"BraTS2021",  "TCGA_IDH",  "TCGA_MGMT"
+    "task_list": [], #["TCGA-LGG_3.201_is1p19qCodeleted_base", "TCGA-LGG_3.202_is1p19qCodeleted_with_clinicalInfo"],  
     "features_for_TCGA": "extracted_features", #"extracted_features", "public_features"
+    "feature_filter_dict":{"modality_list": ["t1", "t1ce", "t2", "flair"], #["t1", "t1Gd", "t2", "flair"], ["t1", "t1ce", "t2", "flair"],
+                         "imageType_list": ["gradient"],
+                                          #["original", "gradient", "log-sigma-1-0-mm-3D", "log-sigma-3-0-mm-3D"],
+                         "tumor_subregion_list": ["NCR", "ED", "ET", "TC", "wholeTumor"], #["NCR", "ED", "ET", "TC", "wholeTumor"]
+                        },
     "feature_selection_method":"AnovaTest", #"RFECV","RFE", AnovaTest, SelectFromModel
     "use_randomSearchCV":True, #False, True
     "harmonization_method": "withoutComBat", # withoutComBat, "parametric_ComBat", nonParametric_ComBat, noEB_ComBat
