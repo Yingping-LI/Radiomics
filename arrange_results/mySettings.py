3229d00d5385223265961b69d3c48182d322408d
liyingping <yingpingleee@126.com>
2021-09-12 23:04:48 +0200

    show the results

---
+++ arrange_results/mySettings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def get_baseresults_path():
+    results_bathpath="G://PhDProjects\RadiogenomicsProjects\GliomasSubtypes\ArrangeResults"
+   
+    return results_bathpath
+
+
