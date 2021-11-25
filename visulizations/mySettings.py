20b7b907bf5e73d7e50d9d3a54b332385fbecbd0
liyingping <yingpingleee@126.com>
2021-11-25 13:30:01 +0100

    Visualize the cross table and category data ration of the data

--- visulizations/mySettings.py
+++ visulizations/mySettings.py
@@ -3,6 +3,7 @@
 
 import os
 
+#============================   Used for main_visualize_images_and_tumors.ipynb =================================
 def get_image_visualization_setting_dict():
     # base path
     base_dataPath="G://PhDProjects/RadiogenomicsProjects/GliomasSubtypes"
@@ -33,3 +34,87 @@ def get_image_visualization_setting_dict():
         image_visualization_setting_dict[setting_name]=image_visualization_setting   
         
     return image_visualization_setting_dict
+
+
+
+
+
+
+#============================ used for main_visualize_variable_ratios.ipynb =================================
+def get_variable_ratio_visualization_setting_dict():
+    # base path
+    base_dataPath="G://PhDProjects/RadiogenomicsProjects/GliomasSubtypes"
+    
+    #define settings for visualizing classification target ratios.
+    variable_ratio_visualization_setting_dict={}
+    
+   #=================== For predicting IDH mutation status ==========================
+    variable_ratio_visualization_setting_dict["TCGA_IDH"]={
+        "data_excel_path": base_dataPath+"/Features/gene_label/TCGA_subtypes_IDH.xlsx",
+        #settings for cacluating cross table.
+        "crosstab_setting_dict": {
+            # Cross table for statistics
+            "overview":
+            {"index": ["Tumor Grade"],
+             "columns": ["IDH.status",  "X1p.19q.codeletion"],
+             "stacked_for_plots":False},
+            # Plot for tumor grade
+            "tumor_grade":
+            {"index": ["Tumor Grade"],
+             "columns": ["Tumor Grade"],
+             "stacked_for_plots":True}, 
+            # plot to show tumor_grade vs. IDH
+            "tumor_grade_vs_IDH":
+            {"index": ["Tumor Grade"],
+             "columns": ["IDH.status"],
+             "stacked_for_plots":False},
+            # plot to show tumor_grade vs. IDH
+            "IDH_vs_1p19q":
+            {"index": ["IDH.status"],
+             "columns": ["X1p.19q.codeletion"],
+             "stacked_for_plots":False},
+            # Plot for IDH mutation status
+            "IDH":
+            {"index": ["IDH.status"],
+             "columns": ["Tumor Grade"],
+             "stacked_for_plots":True},           
+            # Plot for 1p/19q mutation status
+            "1p19q":
+            {"index": ["X1p.19q.codeletion"],
+             "columns": ["Tumor Grade", "IDH.status"],
+             "stacked_for_plots":True},
+        },
+        #settings for ploting the category data.
+        "visualize_category_setting_list":[
+            #{"x": "Tumor Grade", "hue": None},
+            #{"x": "Tumor Grade", "hue": "IDH.status"},
+            ],
+    
+        "save_basepath": base_dataPath+"/Features/gene_label"
+    }
+       
+    #=================== For predicting MGMT methylation status ==========================
+    variable_ratio_visualization_setting_dict["TCGA_MGMT"]={
+        "data_excel_path": base_dataPath+"/Features/gene_label/TCGA_subtypes_MGMT.xlsx",
+        #settings for cacluating cross table.
+        "crosstab_setting_dict": {
+            # Cross table for statistics
+            "tumor_grade":
+            {"index": ["Tumor Grade"],
+             "columns": ["MGMT.promoter.status"],
+             "stacked_for_plots":False},
+            # Plot for MGMT mutation status
+            "MGMT":
+            {"index": ["MGMT.promoter.status"],
+             "columns": ["Tumor Grade"],
+             "stacked_for_plots":True}
+        },
+        #settings for ploting the category data.
+        "visualize_category_setting_list":[
+            #{"x": "MGMT.promoter.status", "hue": None},
+            #{"x": "Tumor Grade", "hue": "MGMT.promoter.status"}
+        ],
+        "save_basepath": base_dataPath+"/Features/gene_label"
+    }
+
+    return variable_ratio_visualization_setting_dict
