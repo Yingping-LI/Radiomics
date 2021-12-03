a0d9c7ea47a9a6784f079ba59f2c545f24163b64
liyingping <yingpingleee@126.com>
2021-12-03 17:45:10 +0100

    optimize the visualization of model selection process

--- arrange_results/mySettings.py
+++ arrange_results/mySettings.py
@@ -10,13 +10,42 @@ def get_arrange_results_settings_dict():
     basepath="G://PhDProjects/RadiogenomicsProjects/GliomasSubtypes/Results_randomseed2021"
     
     arrange_results_settings_dict={}
-    
+
+    #==================== 1: Compare the normalization method =====================
     arrange_results_settings_dict["compare_normalization_methods"]={
         "results_basepath": os.path.join(basepath, "1-compare_normalization_methods"),
         "groupby_column": "task",
         "plot_setting": {"x_column": "classifier", 
                          "hue_column": "normalization_method",
-                         "hue_order": ["no_normalization", "zscore"] #["no_normalization", "zscore", "fcm"]
+                         "hue_order": ["no_normalization", "zscore"],
+                         "ncol": 2,
+                         "exclude_hue_value": ["fcm"]
+                        }
+    }
+    
+    
+    #====================== 2: Compare different image feature extraction strategy ===================================
+    arrange_results_settings_dict["compare_feature_strategy"]={
+        "results_basepath": os.path.join(basepath, "2-compare_feature_strategy"),
+        "groupby_column": "base_task",
+        "plot_setting": {"x_column": "classifier", 
+                         "hue_column": "task_additional_description",
+                         "hue_order": ["WT_base", "WT_withIndicatorColumns", "NCR-TC-WT_base", "NCR-TC-WT_withIndicatorColumns", "NCR-TC-WT-ED-ET_base", "NCR-TC-WT-ED-ET_withIndicatorColumns"],
+                         "ncol": 3,
+                         "exclude_hue_value": ["ShapeFeatureOnly_base", "ShapeFeatureOnly_withIndicatorColumns"]
+                        }
+    }
+    
+    
+    #====================== 3: Compare different image filters ===================================
+    arrange_results_settings_dict["compare_image_filter"]={
+        "results_basepath": os.path.join(basepath, "3-compare_image_filter"),
+        "groupby_column": "task",
+        "plot_setting": {"x_column": "classifier", 
+                         "hue_column": "image_filter",
+                         "hue_order": ["exponential", "square", "lbp-3D",  "gradient", "wavelet", "original", "squareroot", "logarithm", "log-sigma-1-0-mm-3D"],
+                         "ncol": 5,
+                         "exclude_hue_value": ["log-sigma-3-0-mm-3D"]
                         }
     }
     
